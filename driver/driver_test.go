124b376d3ee7ae47e0c45caf92bbb43b54465b33
rolancia <leeth0610@gmail.com>
2021-04-04 04:49:56 +0900

    adds hybrid mode on test

--- driver/driver_test.go
+++ driver/driver_test.go
@@ -2,7 +2,6 @@ package driver
 
 import (
 	"context"
-	"fmt"
 	"github.com/go-redis/redis"
 	"github.com/rolancia/go-ratomic/ratomic"
 	"sync"
@@ -16,36 +15,46 @@ func TestNewRedisDriver(t *testing.T) {
 
 	cli := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
-		PoolSize: 256,
+		PoolSize: 128,
 	})
 
 	redisDriver := NewRedisDriver("lock", cli)
-	ctx = ratomic.WithRatomic(ctx, redisDriver, ratomic.RetryConfig{
-		NumRetry: 10,
-		Delay:    100 * time.Millisecond,
+	ctx = ratomic.WithRatomic(ctx, redisDriver, ratomic.Options{
+		UseFilter:   false,
+		FilterDelay: 5 * time.Millisecond,
+		NumRetry:    100000,
+		Delay:       30 * time.Millisecond,
 	})
 
 	var sum int64 = 0
 	var failed int64 = 0
-	n := 10000
+	n := 1000
 
 	wg := sync.WaitGroup{}
 	wg.Add(n)
 	for i := 0; i < n; i++ {
-		userNum := i / 10
 		go func() {
 			defer wg.Done()
-			key := fmt.Sprintf("user%d", userNum)
+			key := "user"
 			if err := ratomic.Lock(ctx, key); err != nil {
-				if err.Err != ratomic.ErrBusy {
-					t.Log(err)
-				}
+				t.Error(err)
 				atomic.AddInt64(&failed, 1)
+				return
+			}
+			v, err := cli.Get("val").Int()
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			err = cli.Set("val", v+1, 0).Err()
+			if err != nil {
+				t.Error(err)
 				return
 			}
 			atomic.AddInt64(&sum, 1)
 			if err := ratomic.Unlock(ctx, key); err != nil {
 				t.Error(err.Err, err.Hint)
+				return
 			}
 		}()
 	}
--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.15
 
 require (
 	github.com/go-redis/redis v6.15.9+incompatible
-	github.com/rolancia/go-ratomic v0.0.0-20210403133017-f5df1af232e1
+	github.com/rolancia/go-ratomic v0.0.0-20210403194702-bd8b164a79d1
 )
